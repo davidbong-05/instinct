8c2b0f0a5fa11da17a6bafdcf2d4338ed2bcc17e
davidbxs <davidbxs@sains.com.my>
2022-07-21 16:06:04 +0800

    feat: downloadeble file

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,11 +1,31 @@
 <template>
 	<v-container>
-	HI
+		<v-list bg-color="secondary" class="pa-4" rounded>
+			<v-list-item
+				v-for="item in resources"
+				:key="item"
+				:value="item"
+				:href="item.link"
+				download
+			>
+			<v-list-item-icon :icon="item.icon" class="me-2"></v-list-item-icon>
+			{{ item.name }}
+			</v-list-item>
+		</v-list>
 	</v-container>
 </template>
 
 <script>
 	export default {
-		name: "Resources",			
+		name: "Resources",
+		data() {
+			return {
+				resources:[{
+					icon: "mdi-file-pdf-outline",
+					name: "Neo.pdf",
+					link: "/resources/Neo.pdf",
+				}]
+			};
+		},
 	};
 </script>
