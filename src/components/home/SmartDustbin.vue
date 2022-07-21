ca1cd5e66e2d01675e4aaf97d66a28f58f3b2763
davidbxs <davidbxs@sains.com.my>
2022-07-21 23:10:51 +0800

    build: added smart dustbin

---
+++ src/components/home/SmartDustbin.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="mb-4">
+	    <div class="font-weight-bold text-h4 text-center mb-2">
+	    	Smart Dustbin
+	    </div>
+	    <v-img :src="require('@/assets/smart-dustbin/smart-dustbin.jpg')" cover></v-img>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "SmartDustbin",
+	};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
 	<v-container>
+		<SmartDustbin />
 		<ProblemStatement />
 	</v-container>
 </template>
 
 <script>
 	import ProblemStatement from '@/components/home/ProblemStatement.vue'
+	import SmartDustbin from '@/components/home/SmartDustbin.vue'
+
 	export default {
 		name: "Home",	
 		components: {
 			ProblemStatement,
+			SmartDustbin
 		},
 	};
 </script>
