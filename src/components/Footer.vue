9c3f7259d51b84e7b3c4240033eca6e2449de010
davidbxs <davidbxs@sains.com.my>
2022-07-21 14:45:37 +0800

    style: stick to bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-<v-footer class="pa-0">
+<v-footer app bottom fixed class="pa-0">
     <div class="text-center text-white w-100" style="background-color:#4B6F44">
         &copy {{ new Date().getFullYear() }} — Group Instinct. All Rights Reserved
     </div>
