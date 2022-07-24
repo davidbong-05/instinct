dec582bd395387503bcc57e36da783cf533d3be1
davidbxs <davidbxs@sains.com.my>
2022-07-24 11:02:38 +0800

    style: update image to better resolution

--- src/components/home/ProblemStatement.vue
+++ src/components/home/ProblemStatement.vue
@@ -5,17 +5,23 @@
 	<v-carousel cycle hide-delimiters show-arrows="hover" class="mt-3">
 		<v-carousel-item v-for="item in problems" :key="item">
 			<div class="font-weight-bold text-h6 text-tertiary text-center">{{ item.title }}</div>
-			<v-img :src="item.src" cover></v-img>
-			<v-list bg-color="secondary" rounded>
-				<v-list-item 
-				v-for="text in item.text"
-				:key="text"
-				class="py-0"
-				density="compact"
-				>
-					<div class="text-subtitle-2 text-info">{{text}}</div>
-				</v-list-item>
-			</v-list>
+			<v-row>
+				<v-col md="6" sm="6" cols="12">
+					<v-img :src="item.src" md="heights:10px"></v-img>
+				</v-col>
+				<v-col md="6" sm="6" cols="12">
+					<v-list bg-color="secondary" rounded>
+						<v-list-item
+						v-for="text in item.text"
+						:key="text"
+						class="py-0"
+						density="compact"
+						>
+							<div class="text-subtitle-2 text-info">{{text}}</div>
+						</v-list-item>
+					</v-list>
+				</v-col>
+			</v-row>
 		</v-carousel-item>
 	</v-carousel>
 </template>
@@ -29,7 +35,7 @@
 					{
 						title:
 							"People not utilize the rubbish bin properly & Dislike Segregating Their Garbage",
-						src: require("@/assets/problems/problem1.jpg"),
+						src: require("@/assets/problems/problem1.png"),
 						text: [
 							"There is little information provided about waste segregation",
 							"Lack of environment awareness",
@@ -38,14 +44,14 @@
 					},
 					{
 						title: "Dustbin Are Always Overflowing​",
-						src: require("@/assets/problems/problem2.jpg"),
+						src: require("@/assets/problems/problem2.png"),
 						text: [
 							"Inconsistent trash volume making it is hard to maintain"
 						],
 					},
 					{
 						title: "Bad Dustbin Condition",
-						src: require("@/assets/problems/problem3.jpg"),
+						src: require("@/assets/problems/problem3.png"),
 						text: [
 							"Open lid and the smell of garbage will attract wild animal to scavenge and make a mess around the area"
 						],
--- src/components/home/SmartDustbin.vue
+++ src/components/home/SmartDustbin.vue
@@ -3,7 +3,7 @@
 	    <div class="font-weight-bold text-h4 text-center mb-2">
 	    	Smart Dustbin
 	    </div>
-	    <v-img :src="require('@/assets/smart-dustbin/smart-dustbin.jpg')" cover></v-img>
+	    <v-img :src="require('@/assets/smart-dustbin/smart-dustbin.png')" cover></v-img>
 	</div>
 </template>
 
