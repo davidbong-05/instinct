28685464a1fcf523ac6eb47a17fe64d74c98af9d
davidbxs <davidbxs@sains.com.my>
2022-07-24 15:09:32 +0800

    style: added slogan

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,10 @@
 <template>
-<v-footer app bottom fixed class="pa-0">
-    <div class="text-center text-white w-100" style="background-color: #358673">
-        &copy {{ new Date().getFullYear() }} — Group Instinct. All Rights Reserved
-    </div>
-</v-footer>
+	<v-footer app bottom fixed class="pa-0">
+		<div class="text-center text-white w-100" style="background-color: #358673">
+			<blockquote class="font-italic">"Clean Nation with Smart Dustbin"</blockquote>
+			&copy {{ new Date().getFullYear() }} — Group Instinct. All Rights Reserved
+		</div>
+	</v-footer>
 </template>
 
 <script>
