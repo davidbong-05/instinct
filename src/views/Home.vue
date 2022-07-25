8128cfdf6805a17723277a48e04bc95f2499b434
davidbxs <davidbxs@sains.com.my>
2022-07-25 13:59:15 +0800

    fix: unwanted v-row

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,33 @@
 <template>
-	<v-row>
-		<v-container>
-			<SmartDustbin />
-			<v-divider class="my-4"></v-divider>
-			<ProblemStatement />
-			<v-divider class="my-4"></v-divider>
-			<MainFeatures />
-			<v-divider class="my-4"></v-divider>
-			<v-row class="my-2">
-				<v-col md="4" sm="4" cols="12">
-					<div class="font-weight-bold text-h6">Presented by:</div>
-					<div class="d-flex justify-center mt-2">
-						<v-img :src="require('../assets/logo-outlined.png')" height="75"></v-img>
-					</div>
-				</v-col>
-				<v-col md="8" sm="8" cols="12">
-					<div class="font-weight-bold text-h6">Supported by:</div>
-					<div class="d-flex justify-center mt-2">
-						<v-img :src="require('../assets/ytn_logo.png')" height="75"></v-img>
-						<v-img :src="require('../assets/unimas_logo.png')" height="75"></v-img>
-					</div>
-				</v-col>
-			</v-row>
-		</v-container>
-	</v-row>
+	<v-container>
+		<SmartDustbin />
+		<v-divider class="my-4"></v-divider>
+		<ProblemStatement />
+		<v-divider class="my-4"></v-divider>
+		<MainFeatures />
+		<v-divider class="my-4"></v-divider>
+		<v-row class="my-2">
+			<v-col md="4" sm="4" cols="12">
+				<div class="font-weight-bold text-h6">Presented by:</div>
+				<div class="d-flex justify-center mt-2">
+					<v-img
+						:src="require('../assets/logo-outlined.png')"
+						height="75"
+					></v-img>
+				</div>
+			</v-col>
+			<v-col md="8" sm="8" cols="12">
+				<div class="font-weight-bold text-h6">Supported by:</div>
+				<div class="d-flex justify-center mt-2">
+					<v-img :src="require('../assets/ytn_logo.png')" height="75"></v-img>
+					<v-img
+						:src="require('../assets/unimas_logo.png')"
+						height="75"
+					></v-img>
+				</div>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
